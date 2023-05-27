d2c739821d422f0bfe050a02b4b7cfc65953617d
rusakovdv <lifmen007@gmail.com>
2023-05-27 20:48:12 +0500

    lr15

---
+++ src/main/java/com/example/test_rest_service/service/ModifyRequestService.java
@@ -0,0 +1,7 @@
+package com.example.test_rest_service.service;
+
+import com.example.test_rest_service.model.Request;
+
+public interface ModifyRequestService {
+    void modifyRq(Request request);
+}
---
+++ src/main/java/com/example/test_rest_service/service/ModifyUid.java
@@ -0,0 +1,18 @@
+package com.example.test_rest_service.service;
+
+import com.example.test_rest_service.model.Response;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+@Qualifier("ModifyUid")
+public class ModifyUid implements MyModifyService {
+    @Override
+    public Response modify(Response response) {
+        response.setUid("New Uid");
+
+        return response;
+    }
+}
